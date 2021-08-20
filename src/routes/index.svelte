b33e648bf214569204b867565e8e39e225e86b01
hrfounder <hrfounder@gmail.com>
2021-08-20 02:42:42 +0200

    transactions?!

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,48 @@
 <script>
 	import { activeUser, loading } from 'src/utils/stores';
+
 	async function login() {
 		document.getElementById('ual-button').click();
 	}
+
 	async function logout() {
 		window.logOut();
+	}
+	async function handleTransaction() {
+		let actions = [
+			{
+				account: 'dothetokenss',
+				name: 'transfer',
+				authorization: [
+					{
+						actor: $activeUser.accountName,
+						permission: $activeUser.requestPermission
+					}
+				],
+				data: {
+					from: $activeUser.accountName,
+					to: 'jokesonyouyo',
+					quantity: '1.000 SHELL',
+					memo: '123123123'
+				}
+			}
+		];
+
+		let transaction = { actions };
+		console.log(transaction);
+		try {
+			let res = await $activeUser.signTransaction(transaction, {
+				broadcast: true,
+				blocksBehind: 3,
+				expireSeconds: 60
+			});
+
+			setTimeout(() => {
+				console.log('probable success', res);
+			});
+		} catch (err) {
+			console.error(err);
+		}
 	}
 </script>
 
@@ -13,7 +51,10 @@
 	{#if $activeUser.initialLoad}
 		<button on:click={login}>Login</button>
 	{:else}
+		<!-- IF USER IS LOGGED IN AND READY -->
 		<button on:click={logout}>Log out</button>
 		<h3>Welcome {$activeUser.accountName}</h3>
+
+		<button on:click={handleTransaction}>TEST TRANSACTION</button>
 	{/if}
 {/if}
